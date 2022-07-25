15afe1d1ed4144d4ae581ee1aeb13c15aafa09e1
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-07-25 15:58:33 -0300

    resolucao do exercicio bonus 3 sobre objetos e funcoes

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_bonus/Exc3.js
@@ -0,0 +1,47 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+console.log(retorna_quantidade(basket));
+
+function retorna_quantidade(basket){
+
+let cesta = {
+  Melancia: 0,
+  Abacate: 0,
+  Uva: 0,
+  Laranja: 0,
+  Jaca: 0,
+  Pera: 0,
+  Banana: 0,
+}
+
+for (let index = 0; index<basket.length; index++) {
+  comparaPalavra(basket[index], cesta);
+}
+return cesta;
+}
+
+function comparaPalavra(palavra, cesta){
+  for(let key in cesta){
+    if(palavra === key){
+      cesta[key]++;
+    }
+  }
+}
